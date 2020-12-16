2e2d717bbc3e3f56e99c77b4f33c2a2cd0a3f2c8
Dr. Tobias Weltner <tobias@powershell.one>
2020-12-16 16:01:05 +0100

    Create loadScript.ps1

---
+++ PowerShell/loadScript.ps1
@@ -0,0 +1,29 @@
+ param
+    (
+        [Parameter(Mandatory,HelpMessage='Geben Sie die Listingnummer im Format X-Y ein, z.B. 1-2')]
+        [string]
+        $Id
+    )
+    
+    $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12' 
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+    
+    try
+    {
+        $parts = $id.Split('-')
+        if ($parts.Count -ne 2) { throw "Invalid" }
+        $id1= [int]$parts[0]
+        $id2= [int]$parts[1]
+        if (($id1 -lt 1) -or ($id1 -gt 20)) { throw "Invalid" }
+        if (($id2 -lt 1) -or ($id2 -gt 100)) { throw "Invalid" }
+    }
+    catch
+    {
+        Write-Warning "'$id' wurde im falschen Format angegeben."
+        return    
+    }
+    
+    $code = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/TobiasPSP/OReilly/main/PowerShell/$id1-$id2.ps1"
+    
+    $code.Trim() | Set-ClipBoard
+    Write-Host "Listing $id1-$id2 liegt in der Zwischenablage. Fügen Sie es mit STRG+V in Ihre Konsole oder Editor ein."
