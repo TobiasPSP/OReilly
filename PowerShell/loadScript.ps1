13f4ecdf6ee9c2cdf4756195ab7a6653cad5591a
Dr. Tobias Weltner <tobias@powershell.one>
2021-01-30 01:07:46 +0100

    Update loadScript.ps1

--- PowerShell/loadScript.ps1
+++ PowerShell/loadScript.ps1
@@ -1,42 +1,48 @@
- param
-    (
-        [Parameter(Mandatory,HelpMessage='Geben Sie die Listingnummer im Format X.Y ein, z.B. 1.2')]
-        [string]
-        $Id,
+  [CmdletBinding(SupportsShouldProcess)]
+  param
+  (
+    [Parameter(Mandatory,HelpMessage='Geben Sie die Listingnummer im Format X.Y ein, z.B. 1.2')]
+    [string]
+    $Id,
 
-        [Switch]
-        $Run
-    )
+    [Switch]
+    $Run
+  )
     
-    $AllProtocols = [System.Net.SecurityProtocolType]'Tls12' 
-    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+  $ConfirmPreference = $false
     
-    try
+  $AllProtocols = [System.Net.SecurityProtocolType]'Tls12' 
+  [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+    
+  try
+  {
+    $parts = $id.Split('.')
+    if ($parts.Count -ne 2) { throw "Invalid" }
+    $id1= [int]$parts[0]
+    $id2= [int]$parts[1]
+    if (($id1 -lt 1) -or ($id1 -gt 20)) { throw "Invalid" }
+    if (($id2 -lt 1) -or ($id2 -gt 100)) { throw "Invalid" }
+  }
+  catch
+  {
+    Write-Warning "'$id' wurde im falschen Format angegeben."
+    return    
+  }
+    
+  $code = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/TobiasPSP/OReilly/main/PowerShell/Kapitel%20$id1/$id1-$id2.ps1"
+    
+  if ($Run) 
+  { 
+    Write-Host "Listing $id1.${id2}:"
+    [ScriptBlock]$sb = [ScriptBlock]::Create($code.Trim())
+    $sb
+    if ($PSCmdlet.ShouldProcess("Führt das Skript $id1.$id2 aus dem Buch aus.",'Drücken Sie "A" um den Code jetzt auszuführen, oder "N" um abzubrechen.', "Führen Sie nur Code aus, den Sie verstanden haben und von dem Sie genau wissen, was er auslösen wird!"))
     {
-        $parts = $id.Split('.')
-        if ($parts.Count -ne 2) { throw "Invalid" }
-        $id1= [int]$parts[0]
-        $id2= [int]$parts[1]
-        if (($id1 -lt 1) -or ($id1 -gt 20)) { throw "Invalid" }
-        if (($id2 -lt 1) -or ($id2 -gt 100)) { throw "Invalid" }
-    }
-    catch
-    {
-        Write-Warning "'$id' wurde im falschen Format angegeben."
-        return    
-    }
-    
-    $code = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/TobiasPSP/OReilly/main/PowerShell/Kapitel%20$id1/$id1-$id2.ps1"
-    
-    if ($Run) 
-    { 
-      Write-Host "Listing $id1.$id2 wird ausgeführt:"
-      [ScriptBlock]$sb = [ScriptBlock]::Create($code.Trim())
-      $sb
       $sb.Invoke()
     }
-    else
-    {
-      $code.Trim() | Set-ClipBoard
-      Write-Host "Listing $id1.$id2 liegt in der Zwischenablage. Fügen Sie es mit STRG+V in Ihre Konsole oder Editor ein."
-    }
+  }
+  else
+  {
+    $code.Trim() | Set-ClipBoard
+    Write-Host "Listing $id1.$id2 liegt in der Zwischenablage. Fügen Sie es mit STRG+V in Ihre Konsole oder Editor ein."
+  }
